28f35d181332e64cbdd62bef5ffd86d58b80edf4
catintoner <lukyanov.dm.a@gmail.com>
2023-12-03 00:12:52 +0300

    feature template for React

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='page'>
+      <div className='page__container'>
+        hello, world!
+      </div>
+    </div>
+  );
+}
+
+export default App;
