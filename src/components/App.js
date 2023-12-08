e25fb2a89eb2865042ebd9981806819065df8530
catintoner <lukyanov.dm.a@gmail.com>
2023-12-08 22:07:49 +0300

    feature: add routes and handleBack

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import { Route, Routes, useNavigate } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  function handleReturnBack() {
+    navigate.goBack();
+  }
+
   return (
     <div className='page'>
       <div className='page__container'>
-        hello, world!
+        <Routes>
+          <Route exact path='/'>
+            hello, world!
+          </Route>
+
+          <Route path='*'>
+
+          </Route>
+        </Routes>
+
       </div>
     </div>
   );
