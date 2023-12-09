642ec47b05198dc13bc2d89681c1a33dcbe64fbb
catintoner <lukyanov.dm.a@gmail.com>
2023-12-09 15:26:04 +0300

    feature: add header main

--- src/components/NavPanel/NavPanel.jsx
+++ src/components/NavPanel/NavPanel.jsx
@@ -1,38 +1,53 @@
 import React from "react";
 
 import './NavPanel.css';
-import { NavLink } from "react-router-dom";
-
-import header__logo from "../../images/header__logo_700.svg";
+import { NavLink, Link } from "react-router-dom";
 
 function NavPanel() {
   return (
     <>
       <nav className="header__navigation">
-        <ul className="header__links">
+        <ul className="header__links nav__panel">
           <li>
-            <NavLink to="/">
-              Главная
-            </NavLink>
+            <ul className="header__links navigation__bar">
+              <li>
+                <NavLink to="/" className="header__link">
+                  Главная
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/" className="header__link">
+                  О нас
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/" className="header__link">
+                  Акции
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/" className="header__link">
+                  Контакты
+                </NavLink>
+              </li>
+            </ul>
           </li>
           <li>
-            <NavLink to="/">
-              О нас
-            </NavLink>
+            <ul className="header__links">
+              <li>
+                <NavLink className="header__link">
+                  Вход
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="header__link">
+                  Регистрация
+                </NavLink>
+              </li>
+            </ul>
           </li>
           <li>
-            <NavLink to="/">
-              Акции
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/">
-              Контакты
-            </NavLink>
-          </li>
-          <li>
-            <img className="header__logo" src={header__logo} alt="description">
-            </img>
+            <Link to="/" className="header__link header__logo"></Link>
           </li>
         </ul>
 
--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -3,14 +3,30 @@ import React from "react";
 import './PageNotFound.css';
 
 import NavPanel from "../../components/NavPanel/NavPanel";
+import backgroundImage from "../../images/pageNotFound__background_700.png";
 
 function PageNotFound(props) {
   return (
     <>
-      <NavPanel />
-      <button
-      onClick={props.handleGoBack}
-      >TAP TAP</button>
+      <header>
+        <NavPanel />
+      </header>
+      <main>
+        <section className="main__text">
+          <p className="text__section">Ой!</p>
+          <p className="text__section">404</p>
+        </section>
+        <figure className="main__content">
+          <img src={backgroundImage} alt="girl" className="main__image" />
+          <div className="main__textBlock">
+            <figcaption className="main__description">Такой страницы тут нет</figcaption>
+            <button className="main__backButton"
+              onClick={props.handleGoBack}
+            >Вернуться обратно</button>
+          </div>
+        </figure>
+      </main>
+
     </>
   );
 }
